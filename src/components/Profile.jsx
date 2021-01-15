63e7186d63260db6e2d68d630845137e258020c7
Whiteway <whiteway2605@gmail.com>
2021-01-16 00:37:40 +0600

    initial commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <section className="content">
+      <img
+        src="https://www.metoffice.gov.uk/binaries/content/gallery/metofficegovuk/hero-images/advice/maps-satellite-images/satellite-image-of-globe.jpg"
+        alt=""
+      />
+    </section>
+  );
+}
+
+export default Profile;
